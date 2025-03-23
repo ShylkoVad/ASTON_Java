3fd810aa9724a5f74ae989fd603e32c732fdcad0
Sergei <sergeipopov116@icloud.com>
2025-03-23 21:22:53 +0300

    add Shell Sort

---
+++ src/main/java/ru/aston/strategy/ShellSort.java
@@ -0,0 +1,25 @@
+package ru.aston.strategy;
+
+import ru.aston.my_array_list.MyArrayList;
+
+public class ShellSort {
+
+    public static <T extends Comparable<T>> void shellSortComparable(MyArrayList<T> list) {
+        if (list == null || list.isEmpty()) {
+            throw new IllegalArgumentException("List is null or empty");
+        }
+
+        int n = list.size();
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                T temp = list.get(i);
+                int j = i;
+                while (j >= gap && list.get(j - gap).compareTo(temp) > 0) {
+                    list.set(list.get(j - gap), j);
+                    j -= gap;
+                }
+                list.set(temp, j);
+            }
+        }
+    }
+}
