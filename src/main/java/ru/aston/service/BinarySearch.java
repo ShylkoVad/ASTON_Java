960db07e3557b0cdc694f8142e140abac94576a1
Мария Мизина <ivanutkevich@gmail.com>
2025-03-23 11:25:33 +0300

    Generic binary search

--- src/main/java/ru/aston/service/BinarySearch.java
+++ src/main/java/ru/aston/service/BinarySearch.java
@@ -1,20 +1,19 @@
 package ru.aston.service;
 
-import java.util.List;
-
-public class BinarySearch {
-    public static int Search(List<Car> cars, Car findValue){
+public class BinarySearch<T extends Comparable> {
+    public int Search(MyArrayList<T> objects, T findValue){
         int result = -1;
         int low = 0;
-        int high = cars.size() - 1;
+        int high = objects.size() - 1;
         while (low <= high){
             int mid = (low + high) / 2;
-            if(findValue.age > cars.get(mid).age){
+            int compare = findValue.compareTo(objects.get(mid));
+            if(compare > 0){
                 low = mid + 1;
-            } else if (findValue.age < cars.get(mid).age) {
+            } else if (compare < 0) {
                 high = mid - 1;
             }
-            else if(findValue.age == cars.get(mid).age){
+            else {
                 result = mid;
                 break;
             }
